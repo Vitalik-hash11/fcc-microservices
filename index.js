5b9b24edc54bc0b7834b02922cc8a27a8df09cae
newbie <newbie@newbies-MacBook-Pro.local>
2021-06-02 15:03:02 +0300

    create Request Header Parser Microservice

--- index.js
+++ index.js
@@ -7,28 +7,41 @@ app.get("/", (req, res) => {
 
 //timestamp
 
-app.get('/api', (req, res) => {
+app.get("/api/timestamp", (req, res) => {
   const date = new Date();
 
   res.json({
     unix: date.valueOf(),
     utc: date.toUTCString(),
   });
-})
+});
 
-app.get("/api/:time", (req, res) => {
+app.get("/api/timestamp/:time", (req, res) => {
   const { time } = req.params;
 
   let date;
 
-  if (time.includes("-")) {
-    const timeArr = time.split("-");
-    date = new Date(...[timeArr]);
-  } else date = new Date(+time);
-  if (date.toUTCString() === "Invalid Date") res.json({ error: "Invalid Date" });
+  if (time.includes("-")) date = new Date(time);
+  else date = new Date(+time);
+
+  if (date.toUTCString() === "Invalid Date")
+    res.json({ error: "Invalid Date" });
+
   res.json({
     unix: date.valueOf(),
     utc: date.toUTCString(),
+  });
+});
+
+//Request Header Parser Microservice
+
+app.get("/api/whoami", (req, res) => {
+  res.json({
+    ipaddress:
+      (req.headers["x-forwarded-for"] || "").split(",").pop().trim() ||
+      req.socket.remoteAddress,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"],
   });
 });
 
