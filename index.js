90096f965c7b7a3e88edf1cf0a8b30c8f5931a7d
newbie <newbie@newbies-MacBook-Pro.local>
2021-06-02 22:07:54 +0300

    create url shortener microservice

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const MONGO_URI = require("./env.js");
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: "false" }));
+app.use(bodyParser.json());
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+console.log(MONGO_URI);
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const urlSchema = new mongoose.Schema({
+  title: String,
+  shortUrl: Number,
+});
+
+const Url = mongoose.model("Url", urlSchema);
 
 app.get("/", (req, res) => {
   res.send("FreeCodeCamp Microservices");
@@ -45,6 +66,39 @@ app.get("/api/whoami", (req, res) => {
   });
 });
 
-app.listen(4000, () => {
-  console.log(`Listen on port 4000`);
+// URL Shortener Microservice
+
+app
+  .route("/api/shorturl")
+  .get((req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+  })
+  .post((req, res) => {
+    const { url } = req.body;
+    Url.estimatedDocumentCount()
+      .exec()
+      .then((data) => {
+        const link = new Url({ title: url, shortUrl: ++data });
+        link.save((err, url) => {
+          if (err) throw new Error();
+          res.json({ title: url.title, shortUrl: url.shortUrl });
+        });
+      })
+      .catch((err) => console.log(err));
+  });
+
+app.get("/api/shorturl/:shortUrl", (req, res) => {
+  const { shortUrl } = req.params;
+  Url.findOne({ shortUrl }).exec((err, url) => {
+    if (err) throw new Error(err);
+    res.redirect(url.title);
+  });
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  app.listen(4000, () => {
+    console.log(`Listen on port 4000`);
+  });
 });
